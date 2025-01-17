f88e628b9b27d29e0440f6db4eae239a89f8245c
Suqqo <168795338+gayb45@users.noreply.github.com>
2025-01-17 17:46:13 -0300

    fixed links, sim, tudo feito por chatgpt, cuidado

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 let mainClass = document.getElementById("main-class");
 let alpha = 0.1;
 
-function onLoad() {
-    console.log("sexo")
-    interval
-}
-
 setInterval(() => {
     if (alpha < 0.5)
         alpha += 0.001
     mainClass.style.color = 'rgba(206, 206, 206, ' + alpha + ')'
-  }, 1);
+}, 1);
  
- clearInterval(interval); 
+clearInterval(interval); 
+
+function goToPage(page) {
+    switch (page) {
+        case 0:
+            page = 'https://www.youtube.com/watch?v=-t5R5dLty58' // coming soon, fds
+            break;
+        case 1:
+            break;
+        case 2:
+            page = 'https://discord.gg/5qQ5rE3eEN' // biggie
+            break;
+        case 3:
+            page = 'https://github.com/viniciusroger/Raven-BFun' // bfun
+            break
+    }
+    
+    window.open(page)
+} // que bgl estranho da porra, vtmnc, jaja eu me mato aqui.
